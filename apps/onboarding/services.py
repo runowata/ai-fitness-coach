666c42adc7bcc97b8bb36f0a2d4023c72ae54947
runowata <runowata@gmail.com>
2025-07-19 15:34:28 +0300

    Add compressed avatar images and transformation video
    - Move avatars to static/images/avatars/ directory with clean names
    - Update all code references to new avatar paths
    - Allow transformation video in Git while keeping other videos ignored
    - Update onboarding service, restore_data command, and templates

--- apps/onboarding/services.py
+++ apps/onboarding/services.py
@@ -238,21 +238,21 @@ class ArchetypeService:
                 'key': 'bro',
                 'name': 'Бро',
                 'description': 'Дружелюбный и мотивирующий. Всегда поддержит и подскажет. Говорит простым языком.',
-                'image': '/static/images/bro-avatar.png',
+                'image': '/static/images/avatars/bro-avatar.png',
                 'style': 'Casual и расслабленный'
             },
             {
                 'key': 'sergeant',
                 'name': 'Сержант',
                 'description': 'Строгий и требовательный. Поможет вам превзойти себя. Четкие команды и дисциплина.',
-                'image': '/static/images/sergeant-avatar.png',
+                'image': '/static/images/avatars/sergeant-avatar.png',
                 'style': 'Военный стиль'
             },
             {
                 'key': 'intellectual',
                 'name': 'Интеллектуал',
                 'description': 'Научный подход к тренировкам. Детальные объяснения и обоснования.',
-                'image': '/static/images/intellectual-avatar.png',
+                'image': '/static/images/avatars/intellectual-avatar.png',
                 'style': 'Научный подход'
             }
         ]
