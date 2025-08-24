e40ef82ff0d1a6101477c330d3b5598d9a55c193
runowata <runowata@gmail.com>
2025-08-24 04:57:33 +0300

    Revert "chore(workouts): align 0001_initial to current models; extend 0002 squashed replaces"
    This reverts commit 231e800002860ed4e3c17d7174ec36aca69ed154.

--- apps/workouts/migrations/0002_squashed_compat.py
+++ apps/workouts/migrations/0002_squashed_compat.py
@@ -7,25 +7,18 @@ class Migration(migrations.Migration):
         ('workouts', '0001_initial'),
     ]
 
-    # Эта squashed-миграция ЗАМЕНЯЕТ все исторические миграции,
-    # включая те, что применены в продакшене но отсутствуют в репо.
-    # Операции no-op, т.к. прод уже содержит корректную схему.
+    # Эта squashed-миграция ЗАМЕНЯЕТ все миграции 0002-0025,
+    # но не выполняет никаких операций (no-op), т.к. прод уже корректен.
     replaces = [
         ('workouts', '0002_alter_videoclip_type'),
-        ('workouts', '0003_csvexercise_explainervideo_finalvideo_weeklylesson_and_more'),
         ('workouts', '0008_csvexercise_explainervideo'),
         ('workouts', '0010_weeklynotification'),
         ('workouts', '0011_safe_equipment_field_and_models'),
         ('workouts', '0012_add_duration_sec_field'),
-        ('workouts', '0013_finalvideo_exercise_equipment_exercise_poster_image_and_more'),
         ('workouts', '0013_v2_schema'),
-        ('workouts', '0014_clean_v1_legacy'),
-        ('workouts', '0014_finalvideo_weeklylesson_and_more'),
         ('workouts', '0014_state_only'),
-        ('workouts', '0015_add_v2_indexes'),
         ('workouts', '0015_merge_state_only_and_0012'),
         ('workouts', '0016_add_video_provider_fields'),
-        ('workouts', '0016_v2_schema'),
         ('workouts', '0017_add_provider_constraints'),
         ('workouts', '0018_fix_unique_constraint_archetype_field'),
         ('workouts', '0019_remove_videoclip_provider_storage_consistency_and_more'),
